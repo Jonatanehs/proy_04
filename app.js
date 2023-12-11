fda83ca0300d2ef75bcec3abed3179d9037e0e15
Jonatanehs <jehernandez257@soy.sena.edu.co>
2023-12-12 11:37:30 +1300

    se termina de comentarear el codigo

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
+// importa el modulo de colores de node js 
 require('colors');
 
+// la constante matematicas contendra lo importado de matematicas 
 const matematicas = require('./modules/matematicas.js');
 
+// se limpiara la consola 
 console.clear()
 
+// se crea una funcion llamada getRandom
 function getRandom() {
+
+/* se generan numeros decimales aleatorios entre 0 y 1 que se multiplica * 10 
+estos numeros los redondea y los retorna */
   return Math.floor(Math.random() * 10);
 }
 
+// se crea una nueva funcion flecha llamada main 
 const main = () => {
+
+// se imprime un mensaje en consola 
   console.log('************************'.red);
   console.log('*  '.magenta + 'Numeros aleatorios'.bgGreen + '  *'.magenta);
   console.log('************************\n\n'.blue);
+
+// se crean dos variables que contenga la funcion getRandom
   let numero1 = getRandom();
   let numero2 = getRandom();
 
+// se imprime mensajes en la consola 
   console.log('+++++++++++++++++++++++++++++++++'.green);
   console.log('+'.green + '  numeros generados = '.yellow +
     `${[numero1]}`.red + ' , ' + `${[numero2]}`.red + '    *\n'.blue
     + '+'.green + '                               *'.blue);
 
-
+// la constante llamada suma contendra lo que este en matematicas.add con dos argumentos
     const suma = matematicas.add(numero1, numero2);
+
+// la constante llamada suma contendra lo que este en matematicas.substract con dos argumentos
     const resta = matematicas.substract(numero1, numero2);
+
+// la constante llamada suma contendra lo que este en matematicas.multiply con dos argumentos
     const multiplicacion = matematicas.multiply(numero1, numero2);
+
+// la constante llamada suma contendra lo que este en matematicas.divide con dos argumentos
     const division = matematicas.divide(numero1, numero2);
     
+// se imprimen mensajes en la consola 
     console.log('+'.green + '  Suma: '.yellow + `${[numero1]}`.red  + ' + ' +  `${[numero2]}`.red 
         + ' = ' + `${suma} `.red + '            *'.blue);
     console.log('+'.green + '  Resta:'.yellow + `${[numero1]}`.red  + ' - ' +  `${[numero2]}`.red 
--- modules/matematicas.js
+++ modules/matematicas.js
@@ -1,32 +1,47 @@
+// se crea la constante math que contendra objetos vacios
 const Math = {};
 
-const add = (a, b) => {
+// Define la funcion sumar(add)
+function add(a,b) {
   return a + b;
 }
 
-const substract = (a, b) => {
+// define la funcion restar(substract)
+function substract(a,b) {
   return a - b;
 }
 
-const multiply = (a, b) => {
+// define la funcion multiplicar(multiply)
+function multiply(a,b) {
   return a * b;
 }
 
-const divide = (a, b) => {
-  if (b == 0 && a == 0) {
-    return "No Aplica";
-  } else if (b == 0) {
-    return "n/a";
+// define la funcion dividir(divide)
+function divide(a,b) {
+
+/* se crea una condicion if anidada que si b es igual a 0
+y a es igual a 0 retorne un mensaje diciendo No Aplica */
+  if (b == 0 && a == 0)  {
+      return "No Aplica";
+
+// si b es igual a 0 retorne n/a 
+  } else if(b==0){
+  return "n/a";
+
+/* si las condiciones son falsas se crea una variable llamada divisionTot
+contendra la division a /b que retornara divisionTot y aparecera solo un decimal */
   } else {
     let divisionTot = a / b;
     return  divisionTot.toFixed(1);
   }
 }
 
+// Math. contendra las funciones matematicas
 Math.add = add;
 Math.substract = substract;
 Math.multiply = multiply;
 Math.divide = divide;
 
+// exporta lo que contenga Math
 module.exports = Math;
  
